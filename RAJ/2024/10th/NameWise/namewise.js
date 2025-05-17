392cca4816eb32fddd1d532e51e2aa70fcbf4d0a
geetasaini2042 <Geetasaini2042@gmail.com>
2025-05-17 22:07:51 +0530

    Update namewise.js

--- RAJ/2024/10th/NameWise/namewise.js
+++ RAJ/2024/10th/NameWise/namewise.js
@@ -1,7 +1,7 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-const checkAnotherUrl = "https://geetasaini2042.github.io/Results/RAJ/2025/10th/index2.html";
+const checkAnotherUrl = "https://geetasaini2042.github.io/Results/RAJ/2024/10th/NameWise/index.html";
 const user = tg.initDataUnsafe.user;
 
 document.addEventListener("DOMContentLoaded", () => {
