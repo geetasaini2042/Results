44db657917bee4a5819263e5aec0fb9e24298e27
geetasaini2042 <Geetasaini2042@gmail.com>
2025-05-17 17:04:26 +0530

    Update namewise.js

--- RAJ/2025/10th/namewise.js
+++ RAJ/2025/10th/namewise.js
@@ -1,7 +1,6 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-const botToken = "7831738668:AAH7Qc1zYoNd5DrY85kU4EN4GXY01JF91fk";  // इसे backend में रखना चाहिए
 const checkAnotherUrl = "https://geetasaini2042.github.io/Results/RAJ/2025/10th/index2.html";
 const user = tg.initDataUnsafe.user;
 
@@ -16,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 let resultSent = false;
 
 function submitResult() {
-  resultSent = false; // <-- FIXED: reset every time user submits
+  resultSent = false;
 
   const name = document.getElementById("name").value.trim();
   const btn = document.querySelector("button");
@@ -55,8 +54,9 @@ function submitResult() {
 
 function sendResult(name, btn, ad) {
   const sourceUrl = `https://rajasthan-10th-result.indiaresults.com/rj/bser/class-10-result-2024/mname-results.aspx`;
-  const getUrl = `https://sainipankaj12.serv00.net/Result/Namewise/result.php?name=${name}&page=1&url=${encodeURIComponent(sourceUrl)}`;
-  const targetUrl = `https://sainipankaj12.serv00.net/Result/Namewise/result.php?name=${name}&page=1&user_id=${user.id}&url=${encodeURIComponent(sourceUrl)}`;
+  const getUrl = `https://sainipankaj12.serv00.net/Result/Namewise/result.php?name=${encodeURIComponent(name)}&page=1&url=${encodeURIComponent(sourceUrl)}`;
+  const targetUrl = `https://sainipankaj12.serv00.net/Result/Namewise/result.php?name=${encodeURIComponent(name)}&page=1&user_id=${user.id}&url=${encodeURIComponent(sourceUrl)}`;
+
   fetch(getUrl)
     .then(response => {
       if (response.status === 200) {
@@ -78,7 +78,8 @@ function sendResult(name, btn, ad) {
       }
     });
 }
-function resetButton(button, again = false) {
+
+function resetButton(button) {
   button.disabled = false;
   button.innerText = "Check Result";
 }
