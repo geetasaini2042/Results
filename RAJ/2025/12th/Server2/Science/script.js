cdd2c7e8284755dc6cb397eb150b29d526d3f811
geetasaini2042 <Geetasaini2042@gmail.com>
2025-05-22 23:04:41 +0530

    Update script.js

--- RAJ/2025/12th/Server2/Science/script.js
+++ RAJ/2025/12th/Server2/Science/script.js
@@ -63,7 +63,7 @@ function submitResult() {
 }
 
 function sendResultToTelegram(roll, btn, ad) {
-  const sourceUrl = `https://rj-12-science-result.indiaresults.com/rj/bser/class-12-science-result-2024/mrollresult.asp`;
+  const sourceUrl = `https://rj-12-science-result.indiaresults.com/rj/bser/class-12-science-result-2025/mrollresult.asp`;
   const getUrl = `https://sainipankaj12.serv00.net/Result/get.php?roll_no=${roll}&url=${encodeURIComponent(sourceUrl)}`;
   const savePdfUrl = `https://sainipankaj12.serv00.net/savepdf.php?url=${encodeURIComponent(getUrl)}`;
 
